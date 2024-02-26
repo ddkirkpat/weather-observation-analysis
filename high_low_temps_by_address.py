d973eacf8a8b43f478163cbb602edfa2bc81cf45
Dennis D. Kirkpatrick <ddkirkpat@gmail.com>
2024-02-26 13:22:03 -0700

    Additioanl linte recommended changes

--- high_low_temps_by_address.py
+++ high_low_temps_by_address.py
@@ -1,7 +1,6 @@
 
 import requests
 from requests.exceptions import HTTPError
-import json
 from collections import defaultdict
 from datetime import datetime
 from tabulate import tabulate
@@ -50,7 +49,7 @@ def get_closest_station(latitude, longitude):
     except Exception as err:
         print(f'Other error occurred: {err}')
     return (station_id)
-    
+
 def get_high_low_temps_by_day(station_id):
     try:
         stations_url = 'https://api.weather.gov/stations/' + str(station_id) + '/observations'
@@ -61,11 +60,11 @@ def get_high_low_temps_by_day(station_id):
     except Exception as err:
         print(f'Other error occurred: {err}')
     temps_by_timestamp = {}
-    for f in range(len(stations_features)):
+    for f in enumerate(stations_features):
         timestamp_string = stations_features[f]['properties'].get('timestamp')
         temperature = stations_features[f]['properties']['temperature'].get('value')
         # Added conditional due to "null" values in temperature data from NWS API
-        if temperature != None:
+        if temperature is not None:
             temps_by_timestamp[timestamp_string] = temperature
     high_low_temperatures_by_date = defaultdict(lambda: {'high': float('-inf'), 'low': float('inf')})
     for timestamp_string, temperature in temps_by_timestamp.items():
@@ -75,7 +74,7 @@ def get_high_low_temps_by_day(station_id):
             high_low_temperatures_by_date[date]['high'] = temperature
         if temperature < high_low_temperatures_by_date[date]['low']:
             high_low_temperatures_by_date[date]['low'] = temperature
-    return high_low_temperatures_by_date
+    return (high_low_temperatures_by_date)
 
 if __name__ == '__main__':
     #address = str(input('Please enter a US address (ex. 1701 Wynkoop St. Denver, CO): ').strip())
